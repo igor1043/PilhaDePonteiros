563db08687ec3c8282add01be8bb9ec36cad16df
igor vinicius Freitas de Souza <igorviniciusfreitasouza@gmail.com>
2021-03-04 07:37:51 -0300

    Update pilha_inteiros.cpp

--- pilha_inteiros.cpp
+++ pilha_inteiros.cpp
@@ -1,8 +1,6 @@
 /*
-
 Programa em C++ que solicita e armazena em uma pilha numeros inteiros
 enquanto o usuario deseja. Depois imprime o conteudo da pilha.
-
 */
 /*
 * Author:  Igor Vinicius Freitas de Souza
